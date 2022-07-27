e1772054ecb4a873e4a1627c20af6bb416133d5d
hungyp <hieu0985792248@gmail.com>
2022-07-27 11:28:00 +0700

    Chapter 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
-class ApplicationController < ActionController::Base
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def help; end
+
+  def about; end
+
+  def contact; end
+
   before_action :set_locale
 
   private
